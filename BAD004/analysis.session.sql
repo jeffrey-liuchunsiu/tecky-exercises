c101c352f65fbf9da2a6e59b8db00d4df800698a
Jeffrey <jeffrey.liu22@gmail.com>
2022-10-05 18:12:02 +0800

    finished exercise

--- BAD004/analysis.session.sql
+++ BAD004/analysis.session.sql
@@ -2,3 +2,27 @@ SELECT file.user_id,
     COUNT(file.user_id)
 FROM file
 GROUP BY file.user_id;
+select *
+from file
+where owner = 'alex';
+SELECT owner,
+    category,
+    COUNT(owner)
+FROM file
+    Inner join category On file.category_id = category.id
+where owner = 'alex'
+    and category = 'Important'
+GROUP BY owner,
+    category;
+SELECT owner,
+    category,
+    COUNT(owner)
+FROM file
+    Inner join category On file.category_id = category.id
+GROUP BY owner,
+    category;
+SELECT file.user_id,
+    COUNT(file.user_id)
+FROM file
+GROUP BY file.user_id
+HAVING count(file.user_id) > 800;
--- BAD004/analysis.ts
+++ BAD004/analysis.ts
@@ -10,55 +10,65 @@ const knex = Knex(knexConfig);
 
 async function main() {
 
-    let firstResult = await knex.raw(
+  let firstResult = await knex.raw(
        /*sql*/ `SELECT
         file.user_id,
         COUNT(file.user_id) 
       FROM
         file
       GROUP BY file.user_id`
-    )
-    console.log(firstResult.rows);
+  )
+  console.log(firstResult.rows);
 
-    let secondResult = await knex.raw(
+  let secondResult = await knex.raw(
         /*sql*/ `SELECT
          file.category_id,
          COUNT(file.category_id) 
        FROM
          file
        GROUP BY file.category_id`
-    )
-    console.log(secondResult.rows);
+  )
+  console.log(secondResult.rows);
 
-    // let thirdResult = await knex.raw(
-    //     /*sql*/ `SELECT
-    //      file.category_id,
-    //      COUNT(file.category_id) 
-    //    FROM
-    //      file
-    //    GROUP BY file.category_id`
-    // )
-    // console.log(thirdResult.rows);
+  let thirdResult = await knex.raw(
+        /*sql*/ `SELECT owner, category,
+        COUNT(owner)
+        FROM file
+        Inner join category
+            On file.category_id = category.id
+        where owner = 'alex' and category = 'Important'
+        GROUP BY owner, category;`
+  )
+  console.log(thirdResult.rows);
 
-    // const result = await knex
-    //     .select("*")
-    //     .from("users");
-    // console.log(result)
-    // const users:any=[];
-    // users.push({username:"peter",password:""})
-    // users.push({username:"tony",password:""})
-    // users.push({username:"james",password:""})
-    // users.push({username:"dickson",password:""})
-    // const result = await knex.insert(users).into("users");
+  let forthResult = await knex.raw(
+        /*sql*/ `SELECT file.user_id,
+    COUNT(file.user_id)
+    FROM file
+    GROUP BY file.user_id
+    HAVING count(file.user_id) > 700;`
+  )
+  console.log(forthResult.rows);
 
-    // const builder = knex.select("*").from("users");
-    // if(false){
-    //     builder.limit(1)
-    // }else{
-    //     builder.limit(100)
-    // }
-    // const result = await builder;
-    // console.log(result);
-    // knex.destroy();
+  // const result = await knex
+  //     .select("*")
+  //     .from("users");
+  // console.log(result)
+  // const users:any=[];
+  // users.push({username:"peter",password:""})
+  // users.push({username:"tony",password:""})
+  // users.push({username:"james",password:""})
+  // users.push({username:"dickson",password:""})
+  // const result = await knex.insert(users).into("users");
+
+  // const builder = knex.select("*").from("users");
+  // if(false){
+  //     builder.limit(1)
+  // }else{
+  //     builder.limit(100)
+  // }
+  // const result = await builder;
+  // console.log(result);
+  // knex.destroy();
 }
 main()
