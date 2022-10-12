fbf7d0a1226d1b38d1b4d726a6bf39885040a3d9
Jeffrey <jeffrey.liu22@gmail.com>
2022-10-12 17:52:02 +0800

    update CI

--- WSP005/knexfile.ts
+++ WSP005/knexfile.ts
@@ -31,9 +31,9 @@ const config: { [key: string]: Knex.Config } = {
     client: 'postgresql',
     connection: {
       host: process.env.POSTGRES_HOST,
-      database: process.env.TEST_DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
     },
     pool: {
       min: 2,
